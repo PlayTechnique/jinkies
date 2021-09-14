d40a13230839393830db1536a3c62b90a28f3169
Gwyn <gwyn@playtechnique.io>
2021-09-13 20:02:30 -0600

    Fix annoying bug leaving a container named jinkies hanging.

--- build_zone/run_jenkins.sh
+++ build_zone/run_jenkins.sh
@@ -5,6 +5,6 @@ cd ${THIS_SCRIPT_DIR}
 
 source build.env
 
-docker run -d -p ${HOST_JENKINS_PORT}:${CONTAINER_JENKINS_PORT} --name ${CONTAINER_SHORT_NAME} ${IMAGE_TAG}:${IMAGE_VERSION}
+docker run -d --rm -p ${HOST_JENKINS_PORT}:${CONTAINER_JENKINS_PORT} --name ${CONTAINER_SHORT_NAME} ${IMAGE_TAG}:${IMAGE_VERSION}
 
 echo "http://localhost:${HOST_JENKINS_PORT}"
